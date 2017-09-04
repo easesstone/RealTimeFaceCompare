cf86b4e9a4b48e08c2ca9331dcec0017bc46ad1f
lemon <lemon52121@163.com>
2017-09-04 11:05:01 +0800

    FTP动态库数据同步ES

--- FtpServer/src/main/java/com/hzgc/ftpserver/kafka/consumer/WorkerThread.java
+++ FtpServer/src/main/java/com/hzgc/ftpserver/kafka/consumer/WorkerThread.java
@@ -1,6 +1,8 @@
 package com.hzgc.ftpserver.kafka.consumer;
 
 import com.hzgc.ftpserver.util.FtpUtil;
+import com.hzgc.hbase.dynamicrepo.DynamicTable;
+import com.hzgc.hbase.staticrepo.ElasticSearchHelper;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.Durability;
@@ -10,12 +12,11 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.log4j.Logger;
 
-import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
@@ -77,9 +78,16 @@ public class WorkerThread implements Runnable, Serializable {
                     picTable.put(put);
                     LOG.info(Thread.currentThread().getName() + " [topic:" + consumerRecord.topic() +
                             ", offset:" + consumerRecord.offset() +
-                            ", key:" + consumerRecord.key()+
+                            ", key:" + consumerRecord.key() +
                             ", partition:" + consumerRecord.partition() +
                             "]");
+                    //将ipcID与time同步到ES中
+                    Map<String, String> mapES = new HashMap<>();
+                    mapES.put("s", ipcID);
+                    mapES.put("t", dateFormat.format(date));
+                    mapES.put("sj", map.get("sj"));
+                    ElasticSearchHelper.getEsClient().prepareIndex(DynamicTable.DYNAMIC_INDEX, DynamicTable.PERSON_INDEX_TYPE,
+                            consumerRecord.key()).setSource(mapES).get();
                 }
             }
         } catch (Exception e) {
