62abb65b51cecc7f3b26eae80cf55df9cf1d6d76
Administrator@SKY-20170426NWT <personalc@163.com>
2017-10-13 10:08:26 +0800

    modified workerThread.java

--- FtpServer/src/main/java/com/hzgc/ftpserver/kafka/consumer/WorkerThread.java
+++ FtpServer/src/main/java/com/hzgc/ftpserver/kafka/consumer/WorkerThread.java
@@ -4,18 +4,17 @@ import com.hzgc.ftpserver.util.FtpUtil;
 import com.hzgc.hbase.dynamicrepo.DynamicTable;
 import com.hzgc.hbase.staticrepo.ElasticSearchHelper;
 import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.Connection;
-import org.apache.hadoop.hbase.client.Durability;
-import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.log4j.Logger;
-import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
 
 import java.io.IOException;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
@@ -32,15 +31,18 @@ public class WorkerThread implements Runnable, Serializable {
     private String column_pic;
     private String column_ipcID;
     private String column_time;
+    private BulkRequestBuilder bulkRequestBuilder;
+    protected ArrayList<Put> consumer2HBaselist;
+    private  ConsumerRecord<String, byte[]> consumerRecord;
 
     WorkerThread(Connection conn,
-                 BlockingQueue<ConsumerRecord<String, byte[]>> buffer,
-                 String tableName,
-                 String columnFamily,
-                 String column_pic,
-                 String column_ipcID,
-                 String column_time,
-                 ConcurrentHashMap<String, Boolean> commit) {
+          BlockingQueue<ConsumerRecord<String, byte[]>> buffer,
+          String tableName,
+          String columnFamily,
+          String column_pic,
+          String column_ipcID,
+          String column_time,
+          ConcurrentHashMap<String, Boolean> commit) {
         this.buffer = buffer;
         this.hbaseConn = conn;
         this.isCommit = commit;
@@ -57,47 +59,67 @@ public class WorkerThread implements Runnable, Serializable {
     }
 
     private void send() {
+        int i=0;
         try {
             if (null != tableName) {
                 picTable = hbaseConn.getTable(TableName.valueOf(tableName));
             }
             while (true) {
-                ConsumerRecord<String, byte[]> consumerRecord = buffer.take();
-                picTable = hbaseConn.getTable(TableName.valueOf(tableName));
-                if (null != columnFamily && null != column_pic && null != consumerRecord) {
-                    Put put = new Put(Bytes.toBytes(consumerRecord.key()));
-                    put.setDurability(Durability.SYNC_WAL);
-                    put.addColumn(Bytes.toBytes(columnFamily), Bytes.toBytes(column_pic), consumerRecord.value());
-                    Map<String, String> map = FtpUtil.getRowKeyMessage(consumerRecord.key());
-                    String ipcID = map.get("ipcID");
-                    long timestamp = Long.valueOf(map.get("time"));
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                    put.addColumn(Bytes.toBytes(columnFamily), Bytes.toBytes(column_ipcID), Bytes.toBytes(ipcID));
-                    put.addColumn(Bytes.toBytes(columnFamily), Bytes.toBytes(column_time), Bytes.toBytes(dateFormat.format(timestamp)));
-                    picTable.put(put);
-                    LOG.info(Thread.currentThread().getName() + " [topic:" + consumerRecord.topic() +
-                            ", key:" + consumerRecord.key() +
-                            ", offset:" + consumerRecord.offset() +
-                            ", partition:" + consumerRecord.partition() +
-                            "]");
-                    //将ipcID与time同步到ES中(只有人脸图信息同步到ES中)
-                    if (consumerRecord.topic().equals("face")){
-                        Map<String, String> mapES = new HashMap<>();
-                        mapES.put("s", ipcID);
-                        mapES.put("t", dateFormat.format(timestamp));
-                        mapES.put("sj", map.get("sj"));
-                        IndexResponse indexResponse =ElasticSearchHelper.getEsClient().prepareIndex(DynamicTable.DYNAMIC_INDEX, DynamicTable.PERSON_INDEX_TYPE,
-                                consumerRecord.key()).setSource(mapES).get();
-                        LOG.info(Thread.currentThread().getName() + "[topic:" + consumerRecord.topic() +
+                int count=buffer.size();
+                int realNum;
+                if (i < count) {
+                    consumerRecord = buffer.take();
+                    String topic=consumerRecord.topic();
+                    int faceNum=0;
+                    if (null != columnFamily && null != column_pic) {
+                        Put put = new Put(Bytes.toBytes(consumerRecord.key()));//rowkey
+                        put.setDurability(Durability.SKIP_WAL);
+                        put.addColumn(Bytes.toBytes(columnFamily), Bytes.toBytes(column_pic), consumerRecord.value());
+                        Map<String, String> map = FtpUtil.getRowKeyMessage(consumerRecord.key());
+                        String ipcID = map.get("ipcID");
+                        long timestamp = Long.valueOf(map.get("time"));
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                        put.addColumn(Bytes.toBytes(columnFamily), Bytes.toBytes(column_ipcID), Bytes.toBytes(ipcID));
+                        put.addColumn(Bytes.toBytes(columnFamily), Bytes.toBytes(column_time), Bytes.toBytes(dateFormat.format(timestamp)));
+                        consumer2HBaselist.add(put);
+                        LOG.info(Thread.currentThread().getName() + " [topic:" + topic +
                                 ", key:" + consumerRecord.key() +
-                                ", index to ES status: " + indexResponse.status() + "]");
+                                ", offset:" + consumerRecord.offset() +
+                                ", partition:" + consumerRecord.partition() +
+                                "]");
+                        //将ipcID与time同步到ES中(只有人脸图信息同步到bulk中)
+                        if ("face".equals(topic)) {
+                            faceNum++;
+                            Map<String, String> mapES = new HashMap<>();
+                            mapES.put("s", ipcID);
+                            mapES.put("t", dateFormat.format(timestamp));
+                            mapES.put("sj", map.get("sj"));
+                            bulkRequestBuilder = ElasticSearchHelper.getEsClient().prepareBulk();
+                            bulkRequestBuilder.add(ElasticSearchHelper.getEsClient()
+                                    .prepareIndex(DynamicTable.DYNAMIC_INDEX, DynamicTable.PERSON_INDEX_TYPE, consumerRecord.key())
+                                    .setSource(mapES));//批量导入map到bulk
+                            LOG.info("add face to bulk Num："+faceNum);
+                        }
+                        realNum=bulkRequestBuilder.numberOfActions();
+                        LOG.info("sum bulk："+realNum);
                     }
+                    i++;
+                }
+                if (i >=count) {
+                    picTable.put(consumer2HBaselist);
+                    consumer2HBaselist.clear();
+                    BulkResponse bulkItemResponses = bulkRequestBuilder.get();//批量导入到ES
+                    LOG.info(Thread.currentThread().getName() + "[topic:" + consumerRecord.topic() +
+                            ", key:" + consumerRecord.key() +
+                            ", bulk to ES status: " + bulkItemResponses.status() + "]");
+
                 }
             }
-        } catch (Exception e) {
+
+        }catch (Exception e) {
             isCommit.replace("isCommit", false);
             e.printStackTrace();
-        } finally {
+        }finally {
             try {
                 picTable.close();
             } catch (IOException e) {
