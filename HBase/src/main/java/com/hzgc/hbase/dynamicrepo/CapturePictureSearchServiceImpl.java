2ba2c0784b3161ab9c1e3fc0f9b4121990bbc86f
ck0903 <ck@hzgosun.com>
2017-11-03 11:59:00 +0800

    修改格式

--- HBase/src/main/java/com/hzgc/hbase/dynamicrepo/CapturePictureSearchServiceImpl.java
+++ HBase/src/main/java/com/hzgc/hbase/dynamicrepo/CapturePictureSearchServiceImpl.java
@@ -593,6 +593,7 @@ public class CapturePictureSearchServiceImpl implements CapturePictureSearchServ
         return returnresult;
     }
 
+
     @Override
     public SearchResult getCaptureHistory(SearchOption option) {
         option.setSearchType(SearchType.PERSON);
