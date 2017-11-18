67785e8251cfcc12135a9a64db306bcd4f992104
chenyonglong <cylsssb@126.com>
2017-11-18 11:21:02 +0800

    gengxin

---
+++ Service/bin/create-dynamic-table.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+################################################################################
+## Copyright:    HZGOSUN Tech. Co, BigData
+## Filename:     create-dynamic-table.sh
+## Description:  创建动态库表(person_table,mid_table)
+## Version:      1.5.0
+## Author:       qiaokaifeng
+## Created:      2017-11-18
+################################################################################
+
+#set -x
+#---------------------------------------------------------------------#
+#                              定义变量                               #
+#---------------------------------------------------------------------#
+
+cd `dirname $0`
+## bin目录
+BIN_DIR=`pwd`
+cd ..
+DEPLOY_DIR=`pwd`
+## 配置文件目录
+CONF_DIR=${DEPLOY_DIR}/conf
+## Jar 包目录
+LIB_DIR=${DEPLOY_DIR}/lib
+## log 日记目录
+LOG_DIR=${DEPLOY_DIR}/logs
+##  log 日记文件
+LOG_FILE=${LOG_DIR}/create-dynamic-table.log
+## bigdata cluster path
+BIGDATA_CLUSTER_PATH=/opt/hzgc/bigdata
+## bigdata hive path
+SPARK_PATH=${BIGDATA_CLUSTER_PATH}/Spark/spark
+
+## 创建person_table
+${SPARK_PATH}/bin/spark-sql -e "CREATE EXTERNAL TABLE IF NOT EXISTS default.person_table( \
+                                ftpurl        string, \
+                                feature       string, \
+                                eyeglasses    int, \
+                                gender        int, \
+                                haircolor     int, \
+                                hairstyle     int, \
+                                hat           int, \
+                                huzi          int, \
+                                tie           int, \
+                                timeslot      int, \
+                                exacttime     Timestamp, \
+                                searchtype    string) \
+                                partitioned by (date string,ipcid string) \
+                                STORED AS PARQUET \
+                                LOCATION '/user/hive/warehouse/person_table';"
+## 创建mid_table
+${SPARK_PATH}/bin/spark-sql -e "CREATE EXTERNAL TABLE IF NOT EXISTS default.mid_table( \
+                                ftpurl        string, \
+                                feature       string, \
+                                eyeglasses    int, \
+                                gender        int, \
+                                haircolor     int, \
+                                hairstyle     int, \
+                                hat           int, \
+                                huzi          int, \
+                                tie           int, \
+                                timeslot      int, \
+                                exacttime     Timestamp, \
+                                searchtype    string, \
+                                date          string, \
+                                ipcid         string) \
+                                STORED AS PARQUET \
+                                LOCATION '/user/hive/warehouse/mid_table';"
