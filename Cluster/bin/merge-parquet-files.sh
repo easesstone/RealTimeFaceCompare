52073d67850c3f350cfa538b61a954a6bed8620b
easesstone <easesstone@qq.com>
2017-11-20 09:37:40 +0800

    to modified the merge parquet file script and the way to get es client .

--- Cluster/bin/merge-parquet-files.sh
+++ Cluster/bin/merge-parquet-files.sh
@@ -74,7 +74,7 @@ ${LIB_DIR}/kafka-clients-${KAFKA_VERSION}.jar,\
 ${LIB_DIR}/ftp-${RELEASE_VERSION}.jar,\
 ${LIB_DIR}/util-${RELEASE_VERSION}.jar,\
 ${LIB_DIR}/bigdata-api-${RELEASE_VERSION}.jar,\
-${LIB_DIR}/hbase-${RELEASE_VERSION}.jar \
+${LIB_DIR}/service-${RELEASE_VERSION}.jar \
 ${LIB_DIR}/streaming-${RELEASE_VERSION}.jar ${hdfsClusterName} ${tmpTableHdfsPath} ${hisTableHdfsPath} ${tableName} ${dateString}>> ${LOG_FILE} 2>&1 &
 }
 
