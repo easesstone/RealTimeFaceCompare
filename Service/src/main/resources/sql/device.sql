431fc510cdcc2cda6043ce3ac27cb3f819c530ef
sukidasakura <mashencai@126.com>
2017-11-28 15:29:29 +0800

    修改人：马燊偲 修改内容：设备布控预案部分，删除多余的objToDevice表；添加解除设备绑定的注释。 所属模块：Service 合入人：赵喆 合入时间：2017-11-28

--- Service/src/main/resources/sql/device.sql
+++ Service/src/main/resources/sql/device.sql
@@ -25,16 +25,3 @@ create 'device',
 {NAME => 'device', DATA_BLOCK_ENCODING => 'NONE', BLOOMFILTER => 'ROW', REPLICATION_SCOPE => '0', COMPRESSION =>
 'NONE', VERSIONS => '1', MIN_VERSIONS => '1', KEEP_DELETED_CELLS => 'false', BLOCKSIZE => '65536',
  IN_MEMORY => 'true', BLOCKCACHE => 'true'}
-
- #各个对象类型所绑定设备管理
-------------------------
-|   name   |   field   |
-------------------------
-| 对象类型 |   rowKey  |
-------------------------
-|  平台ID  |   ipcID   |
-------------------------
-create 'objToDevice',
-{NAME => 'objType', DATA_BLOCK_ENCODING => 'NONE', BLOOMFILTER => 'ROW', REPLICATION_SCOPE => '0', COMPRESSION =>
-'NONE', VERSIONS => '1', MIN_VERSIONS => '1', KEEP_DELETED_CELLS => 'false', BLOCKSIZE => '65536',
- IN_MEMORY => 'false', BLOCKCACHE => 'false'}
