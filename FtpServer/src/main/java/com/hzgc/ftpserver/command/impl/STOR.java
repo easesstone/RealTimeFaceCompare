6fe3bab6f8826fef0a39c32408b1351edd6d7111
lemon <lemon52121@163.com>
2017-12-20 14:40:22 +0800

    修改MQ发送ftpUrl

--- FtpServer/src/main/java/com/hzgc/ftpserver/command/impl/STOR.java
+++ FtpServer/src/main/java/com/hzgc/ftpserver/command/impl/STOR.java
@@ -174,10 +174,12 @@ public class STOR extends AbstractCommand {
                             String date = map.get("date");
                             String timeSlot = map.get("sj");
 
-                            //拼装ftpUrl
-                            String ftpUrl = FtpUtil.filePath2absolutePath(fileName);
+                            //拼装ftpUrl (带主机名的ftpUrl)
+                            String ftpHostNameUrl = FtpUtil.filePath2absolutePath(fileName);
+                            //获取ftpUrl (带IP地址的ftpUrl)
+                            String ftpIpUrl = FtpUtil.getFtpUrl(ftpHostNameUrl);
                             //发送到rocketMQ
-                            SendResult tempResult = rocketMQProducer.send(ipcID, timeStamp, ftpUrl.getBytes());
+                            SendResult tempResult = rocketMQProducer.send(ipcID, timeStamp, ftpIpUrl.getBytes());
                             rocketMQProducer.send(rocketMQProducer.getMessTopic(), ipcID, timeStamp, tempResult.getOffsetMsgId().getBytes(), null);
 
                             FaceObject faceObject = new FaceObject();
@@ -190,8 +192,8 @@ public class STOR extends AbstractCommand {
                             faceObject.setStartTime(sdf.format(new Date()));
 
                             //发送到kafka
-                            kafkaProducer.sendKafkaMessage(ProducerOverFtp.getFEATURE(), ftpUrl, faceObject);
-                            LOG.info("send to kafka successfully! {}", ftpUrl);
+                            kafkaProducer.sendKafkaMessage(ProducerOverFtp.getFEATURE(), ftpHostNameUrl, faceObject);
+                            LOG.info("send to kafka successfully! {}", ftpHostNameUrl);
                         }
                     }
                 }
