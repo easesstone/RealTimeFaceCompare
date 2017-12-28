36b34e20fdb43c9d11e7a2baccc19e8332b873e1
lemon <lemon52121@163.com>
2017-12-28 14:02:50 +0800

    FTP日志整理

--- FtpServer/src/main/java/com/hzgc/ftpserver/FTP.java
+++ FtpServer/src/main/java/com/hzgc/ftpserver/FTP.java
@@ -1,5 +1,6 @@
 package com.hzgc.ftpserver;
 
+import com.hzgc.ftpserver.common.LoggerConfig;
 import com.hzgc.ftpserver.queue.DataProcess;
 import com.hzgc.util.common.FileUtil;
 import com.hzgc.ftpserver.command.CommandFactoryFactory;
@@ -21,9 +22,9 @@ public class FTP extends ClusterOverFtp {
     /*
       Set the dynamic log configuration file refresh time
      */
-    /*static {
+    static {
         new LoggerConfig();
-    }*/
+    }
 
     @Override
     public void startFtpServer() {
