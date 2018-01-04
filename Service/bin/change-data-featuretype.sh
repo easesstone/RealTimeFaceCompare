ebc9e330f514f065825923fc4cee6909c57ccd4e
cdb156 <15658067087@163.com>
2018-01-04 08:33:22 +0800

    添加根据pkey修改特征值代码、修改项目脚本、修改JDBCUtil打印日志

--- Service/bin/change-data-featuretype.sh
+++ Service/bin/change-data-featuretype.sh
@@ -12,22 +12,27 @@
 #---------------------------------------------------------------------#
 #                              定义变量                               #
 #---------------------------------------------------------------------#
+#set -x
 cd `dirname $0`
 BIN_DIR=`pwd`    ### bin目录
 cd ..
 DEPLOY_DIR=`pwd`
 CONF_DIR=$DEPLOY_DIR/conf                                                                        ### 项目根目录
 LIB_DIR=$DEPLOY_DIR/lib                                                                          ### Jar 包目录
-LIB_JARS=`ls $LIB_DIR|grep .jar| grep -v avro-ipc-1.7.7-tests.jar | grep -v avro-ipc-1.7.7.jar \
-| grep -v spark-network-common_2.10-1.5.1.jar \
+cd ..
+REALTIMECOMPARE_DIR=`pwd`
+COMMON_LIB=${REALTIMECOMPARE_DIR}/common/lib
+LIB_JARS=`ls $LIB_DIR|grep .jar| grep -v avro-ipc-1.7.7-tests.jar | grep -v avro-ipc-1.7.7.jar | grep -v .bak \
+| grep -v spark-network-common_2.10-1.5.1.jar| grep -v service \
  |awk '{print "'$LIB_DIR'/"$0}'|tr "\n" ":"`                                                     ### jar包位置以及第三方依赖jar包，绝对路径
+COMMON_LIB_JARS=`ls $COMMON_LIB |grep .jar |grep -v .bak | awk '{print "'$COMMON_LIB'/"$0}'|tr "\n" ":"`
 LOG_DIR=${DEPLOY_DIR}/logs                                                                       ### log 日记目录
 if [ $# != 2 ]; then
     echo "sh ***.sh pkey  ***.log";
     exit 0;
 fi
 PKEY=$1
-LOG_FILE_NAME=$3
+LOG_FILE_NAME=$2
 
 LOG_FILE=${LOG_DIR}/${LOG_FILE_NAME}      ##  log 日记文件
 
@@ -44,7 +49,7 @@ function change_feature()
     if [ ! -d $LOG_DIR ]; then
         mkdir $LOG_DIR;
     fi
-    java -classpath $CONF_DIR:$LIB_JARS com.hzgc.service.Staticrepo.ChangeFeatureByPkey  $PKEY  | tee -a  ${LOG_FILE}
+    java -classpath $CONF_DIR:$LIB_JARS:$COMMON_LIB_JARS com.hzgc.service.staticrepo.ChangeFeatureByPkey  $PKEY  | tee -a  ${LOG_FILE}
 }
 
 #####################################################################
