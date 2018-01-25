2e3d3079eb0d75ef31b715f03acaaed7d76b4ba5
cdb156 <15658067087@163.com>
2018-01-25 16:45:39 +0800

    修改FindDiffRows工具类

--- FtpServer/src/main/java/com/hzgc/collect/expand/meger/FindDiffRows.java
+++ FtpServer/src/main/java/com/hzgc/collect/expand/meger/FindDiffRows.java
@@ -10,6 +10,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 工具类FindDiffRows中包含以下三个方法：
+ * <p>
+ * getNotProRows：获取集合中未处理的所有行；
+ * getErrProRows：获取集合中处理失败的所有行；
+ * getAllDiffRows：获取集合中不同行；
+ */
 public class FindDiffRows {
     private static Logger LOG = Logger.getLogger(FindDiffRows.class);
 
@@ -21,7 +28,7 @@ public class FindDiffRows {
      */
     public static List<String> getNotProRows(List<String> allRows) {
         List<String> notProList = new ArrayList<>();
-        String tmp;
+        String row;
         if (allRows == null || allRows.size() == 0) {
             LOG.warn("The unionAllRows size is None");
             return notProList;
@@ -35,16 +42,22 @@ public class FindDiffRows {
         } else {
             Collections.sort(allRows);
             for (int i = 1; i < allRows.size() - 2; i++) {
-                tmp = allRows.get(i);
-                String processState = getRowNumber(tmp);
-                if (!processState.equals(getRowNumber(allRows.get(i - 1))) && !processState.equals(getRowNumber(allRows.get(i + 1)))) {
-                    notProList.add(tmp);
+                row = allRows.get(i);
+                String lineNumber = getRowNumber(row);
+                //将日志中一行数据的序号与其上下行的序号进行比较，
+                // 存在相同则表示已处理，没有相同表示未处理
+                if (!lineNumber.equals(getRowNumber(allRows.get(i - 1)))
+                        && !lineNumber.equals(getRowNumber(allRows.get(i + 1)))) {
+                    notProList.add(row);
                 }
             }
+            //判断第一行数据是否已经处理
             if (!getRowNumber(allRows.get(0)).equals(getRowNumber(allRows.get(1)))) {
                 notProList.add(allRows.get(0));
             }
-            if (!getRowNumber(allRows.get(allRows.size() - 1)).equals(getRowNumber(allRows.get(allRows.size() - 2)))) {
+            //判断最后一行数据是否已经处理
+            if (!getRowNumber(allRows.get(allRows.size() - 1))
+                    .equals(getRowNumber(allRows.get(allRows.size() - 2)))) {
                 notProList.add(allRows.get(allRows.size() - 1));
             }
             return notProList;
@@ -52,12 +65,71 @@ public class FindDiffRows {
 
     }
 
+    /**
+     * 获取合并后日志中数据处理失败的集合
+     *
+     * @param allRows 日志合并后的所有行
+     * @return List对象  合并后集合中数据处理失败的集合
+     */
+    public static List<String> getErrProRows(List<String> allRows) {
+        List<String> failList = new ArrayList<>();
+        String tmp;
+        if (allRows == null || allRows.size() == 0) {
+            LOG.warn("The unionAllRows size is None");
+            return failList;
+        } else {
+            Collections.sort(allRows);
+            for (String allRow : allRows) {
+                tmp = allRow;
+                String processState = getProcessState(tmp);
+                //根据状态是否为零判断数据是否处理成功
+                if (!processState.equals("0")) {
+                    failList.add(tmp);
+                }
+            }
+            return failList;
+        }
+    }
+
 
     /**
-     * 获取一行日志中数据处理状态
+     * 获取集合中不同行
      *
-     * @param row 一行数据
-     * @return String        数据在日志中行号
+     * @param allRows 合并后日志集合
+     * @return List对象       返回合并后不同行的集合
+     */
+    public static List<String> getAllDiffRows(List<String> allRows) {
+        List<String> rows = new ArrayList<>();
+        String row;
+        if (allRows == null || allRows.size() == 0) {
+            LOG.warn("The unionAllRows size is None");
+        } else if (allRows.size() == 1) {
+            LOG.info("The unionAllRows size is OnlyOne");
+            rows.add(allRows.get(0));
+        } else {
+            Collections.sort(allRows);
+            for (int i = 1; i < allRows.size() - 2; i++) {
+                row = allRows.get(i);
+                if (!row.equals(allRows.get(i - 1)) && !row.equals(allRows.get(i + 1))) {
+                    rows.add(row);
+                }
+            }
+            if (!allRows.get(0).equals(allRows.get(1))) {
+                rows.add(allRows.get(0));
+            }
+            if (!allRows.get(allRows.size() - 1).equals(allRows.get(allRows.size() - 2))) {
+                rows.add(allRows.get(allRows.size() - 1));
+            }
+
+        }
+        return rows;
+    }
+
+    /**
+     * 获取一行日志中数据在日志中的序号
+     *
+     * @param row 日志中一行数据
+     * @return String        数据在日志中序号
      */
     private static String getRowNumber(String row) {
         String rowNumber = "";
@@ -73,7 +145,7 @@ public class FindDiffRows {
     /**
      * 获取一行日志中数据处理状态
      *
-     * @param row 一行数据
+     * @param row 日志中一行数据
      * @return String        数据处理的状态
      */
     private static String getProcessState(String row) {
@@ -86,75 +158,4 @@ public class FindDiffRows {
         }
         return processState;
     }
-
-    /**
-     * 根据路径合并两个文件的内容
-     *
-     * @param fileReceivePath 日志路径
-     * @param fileProcessPath 日志路径
-     * @return List对象       合并后的结果
-     */
-    public static List<String> getAllRows(String fileReceivePath, String fileProcessPath) {
-        List<String> rows = new ArrayList<>();
-        File fileReceive = new File(fileReceivePath);
-        File fileProcess = new File(fileProcessPath);
-        BufferedReader readerFielReceive = null;
-        BufferedReader readerProcess = null;
-        if (!fileReceive.exists() || !fileProcess.exists()) {
-            LOG.warn("file: " + fileReceive + " or " + fileProcess + "does not exits, please check it...");
-            return rows;
-        }
-        try {
-            readerFielReceive = new BufferedReader(new FileReader(fileReceive));
-            String line;
-            while ((line = readerFielReceive.readLine()) != null) {
-                rows.add(line);
-            }
-            LOG.info("The Number of the lines of" + fileReceive + " is " + rows.size());
-            readerProcess = new BufferedReader(new FileReader(fileProcess));
-            while ((line = readerProcess.readLine()) != null) {
-                rows.add(line);
-            }
-            LOG.info("The Merged Number of the lines of " + fileReceive + fileProcess + "is" + rows.size());
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (readerFielReceive != null && readerProcess != null) {
-                try {
-                    readerFielReceive.close();
-                    readerProcess.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return rows;
-    }
-
-
-    /**
-     * 获取合并后日志中数据处理失败的集合
-     *
-     * @param allRows 日志合并后的所有行
-     * @return List对象  合并后集合中数据处理失败的集合
-     */
-    public static List<String> getFailRows(List<String> allRows) {
-        List<String> failList = new ArrayList<>();
-        String tmp;
-        if (allRows == null || allRows.size() == 0) {
-            LOG.warn("The unionAllRows size is None");
-            return failList;
-        } else {
-            Collections.sort(allRows);
-            for (String allRow : allRows) {
-                tmp = allRow;
-                String processState = getProcessState(tmp);
-                if (!processState.equals("0")) {
-                    failList.add(tmp);
-                }
-            }
-            return failList;
-        }
-    }
-
 }
