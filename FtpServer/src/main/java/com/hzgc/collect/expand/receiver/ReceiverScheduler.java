81d47ae9b369ce0af2a1b1423d0416ba82bf2d17
ck0903 <ck@hzgosun.com>
2018-01-26 12:08:16 +0800

    ReceiverScheduler文件添加备注和报错输出

--- FtpServer/src/main/java/com/hzgc/collect/expand/receiver/ReceiverScheduler.java
+++ FtpServer/src/main/java/com/hzgc/collect/expand/receiver/ReceiverScheduler.java
@@ -27,11 +27,9 @@ public class ReceiverScheduler {
         this.preapreRecvicer();
     }
 
-
     /**
-     * 向Recvicer对象即队列对象集合中的其中一个插入数据,需要考虑线程安全
-     * <p>
-     * 获取到receiver之后存入数据，将数据存入到blockingQueue中，put
+     * 将封装的LogEvent的日志对象调用recevier的putData方法，写入
+     * receiver队列中
      *
      * @param event 封装的数据对象
      */
@@ -43,9 +41,9 @@ public class ReceiverScheduler {
     }
 
     /**
-     * 通过轮询的方式获取一个Recvicer对象
-     * <p>
-     * 从List（container），采用轮询的方式，返回Receiver
+     * 采用依次取用receiver的方法，根据每次调用getReceiver方法
+     * 对pollingCount进行自增，根据pollingCount和receiverNumber
+     * 的余值判断使用哪个receiver
      *
      * @return 返回Recvicer对象
      */
@@ -56,36 +54,39 @@ public class ReceiverScheduler {
     }
 
     /**
-     * 注册还包括指定queueID和日志路径，新建这个receiver的queueid和日志路径
+     * 将receiver注册至container容器中
+     *
+     * @param receiver 参数receiver，注册至容器中
      */
     private void regist(ReceiverImpl receiver) {
         container.add(receiver);
     }
 
     /**
-     * 初始化Recevicer，其相对应的队列以及日志目录
-     * 指定Recevicer----queueID、配置文件中含有的总日志路径
-     * 再次启动时，判断是否存在Receiver
-     * 接收路径示例：/opt/logdata/receive/r-queueid
-     * 处理路径示例：/opt/logdata/process/p-queueid
+     * 根据配置文件中配置的receiverNumber和日志文件地址调用rebanceReceiver方法
+     * 取得对应的queueIdList，根据这些queueId去初始化receiver
      */
     private void preapreRecvicer() {
-        //取得初始化Receiver的数量
         List<String> queueIdList = reblanceRecevicer(conf.getReceiveNumber(), conf.getProcessLogDir());
         if (queueIdList.size() > 0) {
-
             for (String id : queueIdList) {
                 ReceiverImpl receiver = new ReceiverImpl(conf, id);
                 regist(receiver);
                 pool.execute(new ProcessThread(conf, receiver.getQueue(), id));
             }
         } else {
-            LOG.error("");
+            LOG.error("Can not to get the queueIdList,please check your ReceiverNumber and your FileDir!");
         }
     }
 
-
-    //遍历目录下的所有文件，判断和conf中指定的文件的大小,并调整
+    /**
+     * 根据参数receiverNum的数量和对应的日志存放数量，确定初始化receiver时，
+     * receiver的数量和对应的queueId，返回对应queueId的集合
+     *
+     * @param receiverNum   参数receiver的数量
+     * @param processLogDir 参数Log文件存放地址，用来判断receiver的数量
+     * @return 返回经过判断的对应receiver数量的queueID的集合
+     */
     private List<String> reblanceRecevicer(int receiverNum, String processLogDir) {
         List<String> queueIDList = new ArrayList<>();
         File file = new File(processLogDir);
@@ -118,9 +119,11 @@ public class ReceiverScheduler {
                 }
 
             } else {
+                LOG.error("This file directory is null,please check your file dir!");
                 return queueIDList;
             }
         } else {
+            LOG.error("This file directory is not exist,please mkdir the file directory!");
             return queueIDList;
         }
         return queueIDList;
