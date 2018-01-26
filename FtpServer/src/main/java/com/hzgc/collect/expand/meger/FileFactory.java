0953bb92a2bf4dc4b8fcf561b797d9d85d070a8f
sukidasakura <mashencai@126.com>
2018-01-26 15:04:12 +0800

    修改ftp

--- FtpServer/src/main/java/com/hzgc/collect/expand/meger/FileFactory.java
+++ FtpServer/src/main/java/com/hzgc/collect/expand/meger/FileFactory.java
@@ -12,47 +12,31 @@ import java.util.List;
  */
 public class FileFactory {
 
-    private static FileUtil fileUtil = new FileUtil();
+    private  FileUtil fileUtil = new FileUtil();
 
-    private static String processParentDir;
-    private static List<String> allProcessFiles;
-    private static String receiveFilePath;
-
+    private  String processParentDir;
+    private  List<String> allProcessFiles;
 
     //有参构造函数，传入需处理的日志根路径
     public FileFactory(String processLogDir) {
         this.processParentDir = processLogDir;
         setAllProcessFiles();
-
-//        //列出process日志根目录下所有文件
-//        this.allProcessFiles = fileUtil.listAllFileOfDir(processParentDir);
-//        //根据某一个process日志路径，获得对应receive日志路径
-//        this.receiveFilePath = fileUtil.getRecFileFromProFile(this.processParentDir);
     }
 
     /**
      * set方法
+     * 列出process日志根目录下所有文件
      */
 
-    //列出process日志根目录下所有文件
-    private static List<String> setAllProcessFiles() {
+    private  List<String> setAllProcessFiles() {
         return allProcessFiles = fileUtil.listAllFileOfDir(processParentDir);
     }
-
-    //根据某一个process日志路径，获得对应receive日志路径
-//    private static String setReceiveFilePath(String processFilePath) {
-//        return receiveFilePath = fileUtil.getRecFileFromProFile(processFilePath);
-//    }
 
     /**
      * get方法
      */
     public List<String> getAllProcessFiles() {
-        //System.out.println(allProcessFiles);
         return allProcessFiles;
     }
 
-//    public String getReceiveFilePath() {
-//        return receiveFilePath;
-//    }
 }
