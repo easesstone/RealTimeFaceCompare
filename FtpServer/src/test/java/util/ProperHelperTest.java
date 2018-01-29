9c9d428242a470d5644d03df33f4a5c1e608aa90
sukidasakura <mashencai@126.com>
2018-01-29 10:11:45 +0800

    恢复不需要的部分代码

--- FtpServer/src/test/java/util/ProperHelperTest.java
+++ FtpServer/src/test/java/util/ProperHelperTest.java
@@ -10,20 +10,21 @@ public class ProperHelperTest {
 	public void clusterOverFtpProperHelperFun() throws IOException {
 
 		HelperFactory.regist();
-        ClusterOverFtpProperHelper.getPort();
-        ClusterOverFtpProperHelper.getImplicitSsl();
-        ClusterOverFtpProperHelper.getThreadNum();
+		ClusterOverFtpProperHelper.getPort();
+		ClusterOverFtpProperHelper.getImplicitSsl();
+		ClusterOverFtpProperHelper.getThreadNum();
 
-        FTPAddressProperHelper.getUser();
-        FTPAddressProperHelper.getIp();
-        ProducerOverFtpProperHelper.getBootstrapServers();
-        ProducerOverFtpProperHelper.getClientId();
-        ProducerOverFtpProperHelper.getTopicFeature();
-        ProducerOverFtpProperHelper.getKeySerializer();
+		FTPAddressProperHelper.getUser();
+		FTPAddressProperHelper.getIp();
 
-        RocketMQProperHelper.getAddress();
-        RocketMQProperHelper.getTopic();
-        RocketMQProperHelper.getGroup();
+		ProducerOverFtpProperHelper.getBootstrapServers();
+		ProducerOverFtpProperHelper.getClientId();
+		ProducerOverFtpProperHelper.getTopicFeature();
+		ProducerOverFtpProperHelper.getKeySerializer();
+
+		RocketMQProperHelper.getAddress();
+		RocketMQProperHelper.getTopic();
+		RocketMQProperHelper.getGroup();
 
 
 	}
