26bae932ee3bed3cb5ae6bc9a16754bc744b8793
sukidasakura <mashencai@126.com>
2018-01-29 09:36:09 +0800

    修改

--- FtpServer/src/main/java/com/hzgc/collect/expand/util/ClusterOverFtpProperHelper.java
+++ FtpServer/src/main/java/com/hzgc/collect/expand/util/ClusterOverFtpProperHelper.java
@@ -2,6 +2,7 @@ package com.hzgc.collect.expand.util;
 
 import com.hzgc.util.common.FileUtil;
 import org.apache.log4j.Logger;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -11,62 +12,104 @@ import java.util.Properties;
  * 验证其中的配置；读取所需的配置。（马燊偲）
  */
 public class ClusterOverFtpProperHelper extends ProperHelper {
-	private static Logger log = Logger.getLogger(ClusterOverFtpProperHelper.class);
-	private static String properName = "cluster-over-ftp.properties";
-	private static Properties props = new Properties();
-	private static String port;
-	private static String implicitSsl;
-	private static String threadNum;
+    private static Logger log = Logger.getLogger(ClusterOverFtpProperHelper.class);
+    private static Properties props = new Properties();
+    private static String port;
+    private static String implicitSsl;
+    private static String threadNum;
+    private static String capacity;
+    private static String receiveLogDir;
+    private static String processLogDir;
+    private static String receiveNumber;
 
-	static {
-		try {
-			props.load(new FileInputStream(FileUtil.loadResourceFile(properName)));
-			log.info("Load configuration for ftp server from ./conf/cluster-over-ftp.properties");
+    static {
+        String properName = "cluster-over-ftp.properties";
+        try {
+            props.load(new FileInputStream(FileUtil.loadResourceFile(properName)));
+            log.info("Load configuration for ftp server from ./conf/cluster-over-ftp.properties");
 
-			setPort();
-			setImplicitSsl();
-			setThreadNum();
+            setPort();
+            setImplicitSsl();
+            setThreadNum();
+            setCapacity();
+            setReceiveLogDir();
+            setProcessLogDir();
+            setReceiveNumber();
 
-		} catch (IOException e) {
-			e.printStackTrace();
-			log.error("Catch an unknown error, can't load the configuration file" + properName);
-		}
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.error("Catch an unknown error, can't load the configuration file" + properName);
+        }
+    }
 
-	/**
-	 * set方法。验证配置文件中的值是否为符合条件的格式。
-	 */
+    /**
+     * set方法。验证配置文件中的值是否为符合条件的格式。
+     */
 
-	private static String setPort() {
-		return port = verifyPort("listener-port","2121", props, log);
-	}
+    private static void setPort() {
+        port = verifyPort("listener-port", "2121", props, log);
+    }
 
-	private static String setImplicitSsl() {
-		return implicitSsl = verifyBooleanValue("implicitSsl","false", props, log);
-	}
+    private static void setImplicitSsl() {
+        implicitSsl = verifyBooleanValue("implicitSsl", "false", props, log);
+    }
 
-	private static String setThreadNum() {
-		return threadNum = verifyPositiveIntegerValue("thread.number","3", props, log);
-	}
+    private static void setThreadNum() {
+        threadNum = verifyPositiveIntegerValue("thread.number", "3", props, log);
+    }
 
+    private static String setCapacity() {
+        return capacity = verifyPositiveIntegerValue("capacity", String.valueOf(Integer.MAX_VALUE), props, log);
+    }
 
-	/**
-	 * get方法。提供获取配置文件中的值的方法。
-	 */
+    private static String setReceiveLogDir() {
+        return receiveLogDir = verifyCommonValue("receiveLogDir", "/opt/", props, log);
+    }
 
-	public static String getPort() {
-		log.info("Load the configuration listener-port, the value is \"" + port + "\"");
-		return port;
-	}
+    private static String setProcessLogDir() {
+        return processLogDir = verifyCommonValue("processLogDir", "/opt/", props, log);
+    }
 
-	public static String getImplicitSsl() {
-		log.info("Load the configuration implicitSsl, the value is \"" + implicitSsl + "\"");
-		return implicitSsl;
-	}
+    private static String setReceiveNumber() {
+        return receiveNumber = verifyCommonValue("receiveNumber", String.valueOf(0), props, log);
+    }
 
-	public static String getThreadNum() {
-		log.info("Load the configuration thread.number, the value is \"" + threadNum + "\"");
-		return threadNum;
-	}
+    /**
+     * get方法。提供获取配置文件中的值的方法。
+     */
+
+    public static String getPort() {
+        log.info("Load the configuration listener-port, the value is \"" + port + "\"");
+        return port;
+    }
+
+    public static String getImplicitSsl() {
+        log.info("Load the configuration implicitSsl, the value is \"" + implicitSsl + "\"");
+        return implicitSsl;
+    }
+
+    public static String getThreadNum() {
+        log.info("Load the configuration thread.number, the value is \"" + threadNum + "\"");
+        return threadNum;
+    }
+
+    public static String getCapacity() {
+        log.info("Load the configuration capacity, the value is \"" + capacity + "\"");
+        return capacity;
+    }
+
+    public static String getReceiveLogDir() {
+        log.info("Load the configuration receiveLogDir, the value is \"" + receiveLogDir + "\"");
+        return receiveLogDir;
+    }
+
+    public static String getProcessLogDir() {
+        log.info("Load the configuration processLogDir, the value is \"" + processLogDir + "\"");
+        return processLogDir;
+    }
+
+    public static String getReceiveNumber() {
+        log.info("Load the configuration receiveNumber, the value is \"" + receiveNumber + "\"");
+        return receiveNumber;
+    }
 }
-
