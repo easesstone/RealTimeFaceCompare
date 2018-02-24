0d98df5b0ecb3f57e1f6d960a6ea1ba29b18fd77
personalc <personalc@163.com>
2018-02-24 11:04:18 +0800

    Update FileUtil.java

--- Util/src/main/java/com.hzgc.util/common/FileUtil.java
+++ Util/src/main/java/com.hzgc.util/common/FileUtil.java
@@ -1,8 +1,8 @@
 package com.hzgc.util.common;
 
 import org.apache.log4j.Logger;
-import java.io.FileNotFoundException;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.URL;
 
 public class FileUtil {
