726a0cff0a6c99170a4d7330675d82b75d43d19d
pecanNBU <personalc@163.com>
2018-02-26 09:24:19 +0800

    clustering

--- Cluster/src/main/scala/com/hzgc/cluster/clustering/KMeansClustering.scala
+++ Cluster/src/main/scala/com/hzgc/cluster/clustering/KMeansClustering.scala
@@ -1,127 +1,101 @@
-package com.hzgc.kmeans
+import java.sql.Timestamp
+import java.util
+import java.util.{Calendar, Properties}
 
+import com.hzgc.cluster.clustering.PutDataToHBase
 import com.hzgc.cluster.util.PropertiesUtils
-import com.hzgc.util.{FtpFileOpreate, JDBCUtil, PropertiesUtils, SparkSessionSingleton}
+import com.hzgc.dubbo.clustering.ClusteringAttribute
 import org.apache.log4j.Logger
-import org.apache.spark.SparkConf
-import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
+import org.apache.spark.mllib.clustering.KMeans
 import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.SparkSession
 
 import scala.collection.mutable
-import scala.reflect.io.Path
 
 object KMeansClustering {
-  val LOG = Logger.getLogger(KMeansClustering.getClass)
-  val numClusters = 150
+
+  case class Data(id: Long, time: Timestamp, ipc: String, host: String, spic: String, bpic: String)
+
+  val LOG: Logger = Logger.getLogger(KMeansClustering.getClass)
+  val numClusters = 50
   val numIterations = 100000
   var clusterIndex: Int = 0
 
+
   def main(args: Array[String]) {
 
+    val driverClass = "com.mysql.jdbc.Driver"
+    val sqlProper = new Properties()
     val properties = PropertiesUtils.getProperties
+    val clusterNum = properties.getProperty("job.clustering.cluster.number")
+    val iteraterNum = properties.getProperty("job.clustering.iterater.number")
     val appName = properties.getProperty("job.clustering.appName")
     val url = properties.getProperty("job.clustering.mysql.url")
     val tableName = properties.getProperty("job.clustering.mysql.table")
     val timeField = properties.getProperty("job.clustering.mysql.field.time")
+    val ipcField = properties.getProperty("job.clustering.mysql.field.ipc")
     val dataField = properties.getProperty("job.clustering.mysql.field.data")
     val idField = properties.getProperty("job.clustering.mysql.field.id")
+    val hostField = properties.getProperty("job.clustering.mysql.field.host")
+    val spicField = properties.getProperty("job.clustering.mysql.field.spic")
+    val bpicField = properties.getProperty("job.clustering.mysql.field.bpic")
+    val partitionNum = properties.getProperty("job.clustering.partiton.number").toInt
 
-    val sparkConf = new SparkConf().setAppName(appName)
-    val spark = SparkSession.builder().config(sparkConf).getOrCreate()
+    val spark = SparkSession.builder().appName(appName).enableHiveSupport().master("local[*]").getOrCreate()
+    import spark.implicits._
 
-    val mysqlData = spark.read.format("jdbc").option("url", url).option("dbtable", tableName)
+    val calendar = Calendar.getInstance()
+    val currentYearMon = "'" + calendar.get(Calendar.YEAR) + "-%" + (calendar.get(Calendar.MONTH) + 1) + "%'"
 
+    spark.sql("select ftpurl,feature from person_table where date like " + currentYearMon).createOrReplaceTempView("parquetTable")
 
-    //如何按条件读取parquet文件需要修改
-    //hdfs路径在本地运行时需要添加端口号
-    //通过union方式来读取多个文件
-    /* sparkSession.read.parquet("hdfs://hzgc:9000/user/hive/warehouse/person_table/date=2017-12-13")
-       .union(sparkSession.read.parquet("hdfs://hzgc:9000/user/hive/warehouse/person_table/date=2017-12-09"))
-       .union(sparkSession.read.parquet("hdfs://hzgc:9000/user/hive/warehouse/person_table/date=2017-12-07"))
-       .union(sparkSession.read.parquet("hdfs://hzgc:9000/user/hive/warehouse/person_table/date=2017-12-08"))
-       .select(ftpUrlColumn, featureColumn)
-       .createOrReplaceTempView(tempViewName)*/
+    val preSql = "(select T1.id, T2.host_name, T2.big_picture_url, T2.small_picture_url, T1.alarm_time " + "from t_alarm_record as T1 inner join t_alarm_record_extra as T2 on T1.id=T2.record_id " + "where T2.static_id IS NULL " + "and DATE_FORMAT(T1.alarm_time,'%Y-%m') like " + currentYearMon + ") as temp"
 
-    //采用where条件语句，但是这样很明显spark会先扫描person_table下所有文件，然后再根据where中的条件过滤
-    /* sparkSession.read.parquet("hdfs://hzgc:9000/user/hive/warehouse/person_table").where("date='2017-12-08'")
-       .select(ftpUrlColumn, featureColumn)
-       .createOrReplaceTempView(tempViewName)*/
-    //通过设置option中basePath 来读取多个文件
-    sparkSession.read.option("basePath", basePath).parquet(basePath + "date=2017-12-*") //.where("date='2017-12-08'")
-      .select(ftpUrlColumn, featureColumn)
-      .createOrReplaceTempView("tempView")
-    sparkSession.read.jdbc()
+    sqlProper.setProperty("driver", driverClass)
+    val dataSource = spark.read.jdbc(url, preSql, sqlProper)
+    dataSource.map(data => {
+      println("ftp://" + data.getAs[String](hostField) + ":2121/" + data.getAs[String](spicField))
+      Data(data.getAs[Long](idField), data.getAs[Timestamp](timeField), data.getAs[String](spicField).substring(1, data.getAs[String](spicField).indexOf("/", 1)), data.getAs[String](hostField), "ftp://" + data.getAs[String](hostField) + ":2121" + data.getAs[String](spicField), "ftp://" + data.getAs[String](hostField) + ":2121" + data.getAs[String](bpicField))
+    }).createOrReplaceTempView("mysqlTable")
+    //    }).createOrReplaceTempView("mysqlTable")
+    val joinData = spark.sql("select T1.feature, T2.* from parquetTable as T1 inner join mysqlTable as T2 on T1.ftpurl=T2.spic")
 
-    //udf for feature is null distinct
-    sparkSession.udf.register("len", (x: mutable.WrappedArray[Float]) => x.length)
-    //get data from DataFrame
-    val resultDF = sparkSession.sql("select " + ftpUrlColumn + "," + featureColumn + " from tempView where len(" + featureColumn + ")=" + featureLength)
+    val idPointRDD = joinData.rdd.map(data => (data.getAs[String]("spic"), Vectors.dense(data.getAs[mutable.WrappedArray[Float]]("feature").toArray.map(_.toDouble)))).cache()
+    val kMeansModel = KMeans.train(idPointRDD.map(_._2), numClusters, numIterations)
+    val trainMidResult = kMeansModel.predict(idPointRDD.map(_._2))
+    var trainResult = trainMidResult.zip(joinData.select("id", "time", "ipc", "host", "spic", "bpic").rdd).groupByKey().map(data => (data._1, data._2.toList.sortWith((a, b) => a.getTimestamp(1).getTime > b.getTimestamp(1).getTime))).map(data => (data._1, data._2.toArray.sortWith((a, b) => a.getTimestamp(1).getTime > b.getTimestamp(1).getTime)))
 
-    val idPointRDD = resultDF.rdd.map(s => (s.getString(0), Vectors.dense(s.get(1).asInstanceOf[mutable.WrappedArray[Float]].toArray.map(_.toDouble)))).cache()
-    val startTime = System.nanoTime
+    val table1List = new util.ArrayList[ClusteringAttribute]()
+    trainResult.map(data => {
+      val attribute = new ClusteringAttribute()
+      attribute.setClusteringId(data._1.toString)
+      attribute.setCount(data._2.length)
+      attribute.setFirstAppearTime(data._2(0).getTimestamp(1).toString)
+      attribute.setFirstIpcId(data._2(0).getAs[String]("ipc"))
+      attribute.setLastAppearTime(data._2(data._2.length - 1).getTimestamp(1).toString)
+      attribute.setLastIpcId(data._2(data._2.length - 1).getAs[String]("ipc"))
+      attribute.setFtpUrl(data._2(data._2.length / 2).getAs[String]("spic"))
+      attribute
+    }).collect().foreach(data => table1List.add(data))
 
-    val clusters: KMeansModel = KMeans.train(idPointRDD.map(_._2), numClusters, numIterations)
-    val clustersRDD = clusters.predict(idPointRDD.map(_._2))
-    val endTime = System.nanoTime
-    val idClusterRDD = idPointRDD.map(_._1).zip(clustersRDD)
-    //import sparkSession.sql
-    import sparkSession.implicits._
-    val idClusterDF = idClusterRDD.toDF(ftpUrlColumn, clusterColumn)
-    idClusterDF.repartition($"cluster").foreachPartition(
-      clusterPartition => {
-        clusterPartition.foreach(s => {
-          val ftpUrl = s.getAs[String](0)
-          val clusterName = s.getAs[Int](1)
-          val path: Path = Path(clusterOut + clusterColumn + clusterName)
-          val currentPath = path.createDirectory(failIfExists = false)
-          val pathStr = currentPath.toAbsolute.toString()
-          FtpFileOpreate.downloadFtpFile(ftpUrl, pathStr, ftpUrl.substring(ftpUrl.lastIndexOf("/")))
-        })
-      }
-    )
-
-    println("*****************************************")
-    println("聚类时间为：" + (endTime - startTime) / 1000000d)
-    //Save and load model
-    clusters.save(sc, "target/org/apache/spark/KMeansExample/KMeansModel")
-    val sameModel = KMeansModel.load(sc, "target/org/apache/spark/KMeansExample/KMeansModel")
-    sparkSession.stop()
-  }
-
-  /**
-    * run kmeans and print of Sum of Squared Errors and center of every clusters
-    *
-    **/
-  def transData(resultDF: DataFrame): Unit = {
-    val parsedData = resultDF.rdd.map(s => Vectors.dense(s.get(1)
-      .asInstanceOf[mutable.WrappedArray[Float]]
-      .toArray.map(_.toDouble))).cache()
-    val clusters: KMeansModel = KMeans.train(parsedData, numClusters, numIterations)
-    val WSSSE = clusters.computeCost(parsedData)
-    println("Within Set Sum of Squared Errors = " + WSSSE)
-    println("Cluster Number:" + clusters.clusterCenters.length)
-    println("Cluster Centers Information Overview:")
-    clusters.clusterCenters.foreach(x => {
-      println("Center Point of Cluster " + clusterIndex + ":")
-      println(x)
-      clusterIndex += 1
-    })
-  }
-
-  /**
-    * Query Data With Java（JDBC）
-    */
-  def getDataByJDBC(): Unit = {
-    val conn = JDBCUtil.getConnection
-    val statement = conn.createStatement()
-    //UDF需要打成jar包注册到集群上
-    val resultSet = statement.executeQuery("select ftpurl,feature from person_table limit 10")
-    if (resultSet != null) {
-      while (resultSet.next()) {
-        println(resultSet.getString("ftpurl") + ":" + resultSet.getString("feature"))
-      }
+    val mon = calendar.get(Calendar.MONTH) + 1
+    var monStr = ""
+    if (mon < 10) {
+      monStr = "0" + mon
     }
+    val yearMon = calendar.get(Calendar.YEAR) + "-" + monStr
+    PutDataToHBase.putClusteringInfo(yearMon, table1List)
+
+    trainResult.foreach(data => {
+      val rowkey = yearMon + "-" + data._1
+      println(rowkey)
+      val idList = new util.ArrayList[Integer]()
+      data._2.foreach(data => idList.add(data.getAs[Long]("id").toInt))
+      println(idList)
+      println("++++++++++++++++++++++")
+      PutDataToHBase.putDetailInfo_v1(rowkey, idList)
+    })
+    spark.stop()
   }
 }
 
