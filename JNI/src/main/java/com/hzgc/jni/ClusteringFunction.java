b70d0393da638d49c591085c05f8cbba3a252d95
pecanNBU <personalc@163.com>
2018-03-24 09:33:37 +0800

    modified JNI

--- JNI/src/main/java/com/hzgc/jni/ClusteringFunction.java
+++ JNI/src/main/java/com/hzgc/jni/ClusteringFunction.java
@@ -8,11 +8,12 @@ public class ClusteringFunction implements Serializable {
      * 聚类计算
      *
      * @param features features array
+     * @param dataSize 特征条数
      * @param fileName file name year-month.txt(2018-02.txt)
      * @param filePath absolute path of the results file
      * @return 0 or 1，1 success，0 fail
      */
-    public static native int clusteringComputer(float[] features, String fileName, String filePath);
+    public static native int clusteringComputer(float[] features, int dataSize, String fileName, String filePath);
 
     static {
         System.loadLibrary("ClusteringLib");
