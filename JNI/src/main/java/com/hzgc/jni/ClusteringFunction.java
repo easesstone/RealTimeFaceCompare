d9e82787e919b5dafd681ddd17328630b3576923
pecanNBU <personalc@163.com>
2018-03-24 09:37:50 +0800

    new JNI

--- JNI/src/main/java/com/hzgc/jni/ClusteringFunction.java
+++ JNI/src/main/java/com/hzgc/jni/ClusteringFunction.java
@@ -5,13 +5,13 @@ import java.io.Serializable;
 
 public class ClusteringFunction implements Serializable {
     /**
-     * 聚类计算
+     * permanent resident population calculate
      *
      * @param features features array
-     * @param dataSize 特征条数
+     * @param dataSize features count
      * @param fileName file name year-month.txt(2018-02.txt)
      * @param filePath absolute path of the results file
-     * @return 0 or 1，1 success，0 fail
+     * @return 0 or 1, 1 success, 0 fail
      */
     public static native int clusteringComputer(float[] features, int dataSize, String fileName, String filePath);
 
